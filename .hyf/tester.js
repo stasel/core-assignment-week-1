3c6b69aff1584cd824d00253aafc2233abc3aa76
stasel <2033301+stasel@users.noreply.github.com>
2025-12-24 14:39:12 +0100

    Added testers

--- .hyf/tester.js
+++ .hyf/tester.js
@@ -1,15 +1,22 @@
-import fs from 'fs/promises';
-
-const task1 = async () => {
-
-}
-
-const task2 = async () => {
-
-}
+import { task1 } from './task1.js';
+import { task2 } from './task2.js';
 
 const main = async () => {
+  console.log('======================= Task 1 =======================');
+  const score1 = await task1();
 
+  console.log('\n======================= Task 2 =======================');
+  const score2 = await task2();
+
+  console.log('\n======================= Summary =======================');
+  const totalScore = Math.min(100, Math.round(score1 * 0.7 + score2 * 0.3));
+  console.log(`- Task 1 Score: ${score1}%`);
+  console.log(`- Task 2 Score: ${score2}%`);
+  console.log(`Total Score: ${totalScore}%`);
 }
 
-await main();
+try {
+  await main();
+} catch (error) {
+  console.error(`❌ Something went wrong:\n${error}`);
+}
